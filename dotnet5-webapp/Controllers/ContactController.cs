5a587f0a5e73654c8ce4e1bcff7da245ec8f8850
Santosh Gade <santosh.gade@parallelminds.in>
2020-10-15 16:54:14 +0530

    first commit

---
+++ dotnet5-webapp/Controllers/ContactController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet5_webapp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace dotnet5_webapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private List<Contact> contacts = new List<Contact>
+        {
+            new Contact{Id=1,FirstName="Santosh",LastName="Gade",NickName="SAN",Place="Nashik"},
+             new Contact{Id=2,FirstName="Mayur",LastName="Nathe",NickName="Mayu",Place="Pune"}
+        };
+        // GET: api/<ContactController>
+        [HttpGet]
+        public ActionResult< IEnumerable<Contact>> Get()
+        {
+            return contacts;
+        }
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/<ContactController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Contact> Get(int id)
+        {
+            Contact contact= contacts.FirstOrDefault(e => e.Id == id);
+            if(contact==null)
+            {
+                return NotFound(new { Message="contact has not been found." });
+            }
+            return Ok(contact);
+        }
+
+        // POST api/<ContactController>
+        [HttpPost]
+        public ActionResult<IEnumerable <Contact>> Post(Contact newContact)
+        {
+            contacts.Add(newContact);
+            return contacts;
+        }
+
+        // PUT api/<ContactController>/5
+        [HttpPut("{id}")]
+        public ActionResult<IEnumerable<Contact>> Put(int id, Contact updatedContact)
+        {
+            Contact contact = contacts.FirstOrDefault(c => c.Id == id);
+            if (contact == null)
+            {
+                return NotFound(new { Message = "contact has not been found." });
+            }
+            contact.NickName = updatedContact.NickName;
+            contact.IsDeleted = updatedContact.IsDeleted;
+            return Ok(contact);
+        }
+
+        // DELETE api/<ContactController>/5
+        [HttpDelete("{id}")]
+        public ActionResult<IEnumerable<Contact>> Delete(int id)
+        {
+            Contact contact = contacts.FirstOrDefault(c => c.Id == id);
+            if (contact == null)
+            {
+                return NotFound(new { Message = "contact has not been found." });
+            }
+            contacts.Remove(contact);
+            return contacts;
+        }
+    }
+}
---
+++ dotnet5-webapp/Controllers/ContactsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using dotnet5_webapp.Data;
+using dotnet5_webapp.Models;
+
+namespace dotnet5_webapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ContactsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Contacts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> GetContacts()
+        {
+            //return await _context.Contacts.ToListAsync();
+            return await _context.Contacts.Where(c => c.IsDeleted == false).ToListAsync();
+        }
+
+        // GET: api/Contacts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contact>> GetContact(int id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return contact;
+        }
+
+        // PUT: api/Contacts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContact(int id, Contact contact)
+        {
+            var dbContact = await _context.Contacts.FindAsync(id);
+            if (dbContact == null)
+            {
+                return NotFound();
+            }
+            dbContact.FirstName = contact.FirstName;
+            dbContact.LastName = contact.LastName;
+            dbContact.NickName = contact.NickName;
+            dbContact.Place = contact.Place;
+            await _context.SaveChangesAsync();
+            return Ok(dbContact);
+            //if (id != contact.Id)
+            //{
+            //    return BadRequest();
+            //}
+            //_context.Entry(contact).State = EntityState.Modified;
+            //try
+            //{
+            //    await _context.SaveChangesAsync();
+            //}
+            //catch (DbUpdateConcurrencyException)
+            //{
+            //    if (!ContactExists(id))
+            //    {
+            //        return NotFound();
+            //    }
+            //    else
+            //    {
+            //        throw;
+            //    }
+            //}
+            //return NoContent();
+        }
+
+        // POST: api/Contacts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Contact>> PostContact(Contact contact)
+        {
+            _context.Contacts.Add(contact);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetContact", new { id = contact.Id }, contact);
+            return Ok(_context.Contacts);
+        }
+
+        // DELETE: api/Contacts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteContact(int id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            //_context.Contacts.Remove(contact);
+            contact.IsDeleted = true;
+            await _context.SaveChangesAsync();
+            return Ok(_context.Contacts);
+
+            //return NoContent();
+        }
+
+        private bool ContactExists(int id)
+        {
+            return _context.Contacts.Any(e => e.Id == id);
+        }
+    }
+}
